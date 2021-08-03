16a5bc3d38604c241693c55997992dba38aaa524
Andre <andredelima92@gmail.com>
2021-08-03 17:07:55 -0300

    add dot env

--- mobile/src/services/api.tsx
+++ mobile/src/services/api.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_URL } from '@env';
 
 const api = axios.create({
-  baseURL: 'http://192.168.3.36/parlador-ideal/server/public/api',
+  baseURL: API_URL,
 });
 
 export default api;
