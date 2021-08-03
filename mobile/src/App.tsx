2dfb56995ae2f4f498c1d93df748a174184c29e1
Andre <andredelima92@gmail.com>
2021-08-03 17:00:33 -0300

    add splash screen

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,11 +1,18 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import Routes from './Routes/index.routes';
+import RNBootSplash from 'react-native-bootsplash';
 
 import { Container } from './styles';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      RNBootSplash.hide();
+    }, 1000);
+  }, []);
+
   return (
     <Container>
       <StatusBar />
